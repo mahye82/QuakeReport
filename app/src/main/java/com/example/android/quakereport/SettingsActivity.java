31e9dc57fb25e75d5c516b01d374d32f110f383e
MA Hye <mahye.82@gmail.com>
2016-10-11 17:04:51 +0100

    Add a min magnitude Preference to a PreferenceScreen

--- app/src/main/java/com/example/android/quakereport/SettingsActivity.java
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -13,6 +13,14 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     public static class EarthquakePreferenceFragment extends PreferenceFragment {
-
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            // Inflate the settings_main.xml which contains a PreferenceScreen root View.
+            // This creates a list of Preferences which can be clicked and edited, with the
+            // value inputted by the user then stored with an associated key.
+            // Currently, there is only one Preference for 'Minimum Magnitude.
+            addPreferencesFromResource(R.xml.settings_main);
+        }
     }
 }
